c60c700e44172dacce693dda8b8877f2e6ee2fef
Serhiy Kovalchuk <kovalchuk@RACECLUB.ES>
2023-10-16 16:55:22 +0200

    primeros pasos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+//import Button from "./components/button/Button";
+import Title from "./components/title/Title";
+import Subtitle from "./components/subtitle/Subtitle";
+import Text from "./components/text/Text";
+
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	return (
+		<>
+			<Title text="Soy un Title" />
+			<Subtitle text="Soy un Subtitle" />
+			<Text text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi deleniti explicabo ut sapiente asperiores, eos illum eum adipisci aspernatur nulla, numquam quae amet tempore? Ipsam at repellat esse quasi consectetur?" />
+		</>
+	);
+	
 };
+
+
+//console.log(App());
 
 export default App;
